07230dca45a1b089309de35c27a9b6e9b0e64b59
classAndrew <andrxwlin@gmail.com>
2020-12-24 17:46:32 -0800

    Add Camera rotation pitch

--- index.js
+++ index.js
@@ -24,7 +24,18 @@ const PREC = 80;
 
 */
 function main() {
-    beginRender()
+    beginRender();
+}
+
+// get normalized vector
+function normalized(vec) {
+    const len = Math.sqrt(vec[0] ** 2 + vec[1] ** 2 + vec[2] ** 2);
+    return [vec[0] / len, vec[1] / len, vec[2] / len];
+}
+
+// get vector (cross) product 
+function cross(vec1, vec2) {
+    return [vec1[1] * vec2[2] - vec1[2] * vec2[1], -(vec1[0] * vec2[2] - vec1[2] * vec2[0]), vec1[0] * vec2[1] - vec1[1] * vec2[0]];
 }
 
 class Camera {
@@ -32,10 +43,12 @@ class Camera {
     x = 2;
     y = 1.3;
     pitch = 0;
-    yaw = 0;
+    yaw = PI / 2;
     speed = 0.005;
     dir = [0, 0, 1];
-    up = [0, 1, 0]
+    worldUp = [0, 1, 0];
+    up = this.worldUp;
+    right = normalized(cross(this.up, this.dir));
     constructor(x, y, z, pitch, yaw, speed) {
         Object.assign(this, { x, y, z, pitch, yaw, speed });
     }
@@ -43,22 +56,41 @@ class Camera {
     getViewMat() {
         // find lookat matrix
         let view = new Mat4();
-        view.m[0][0] = pitch;
-        view.m[0][2] = yaw;
-        view.m[1][0] = up[0];
-        view.m[1][1] = up[1];
-        view.m[1][2] = up[2];
-        view.m[2][0] = dir[0];
-        view.m[2][1] = dir[1];
-        view.m[2][2] = dir[2];
+        view.m[0][0] = this.right[0];
+        view.m[0][1] = this.right[1]
+        view.m[0][2] = this.right[2];
+        view.m[1][0] = this.up[0];
+        view.m[1][1] = this.up[1];
+        view.m[1][2] = this.up[2];
+        view.m[2][0] = this.dir[0];
+        view.m[2][1] = this.dir[1];
+        view.m[2][2] = this.dir[2];
         view.m[3][3] = 1;
         let coeMat = Mat4.identity();
         coeMat.m[0][3] = -this.x;
-        coeMat.m[1][3] = -this.x;
-        coeMat.m[2][3] = -this.x;
-        return view.mul(coeMat);
+        coeMat.m[1][3] = -this.y;
+        coeMat.m[2][3] = -this.z;
+        return view.mmul(coeMat);
     }
 
+    updateVecs() {
+        this.dir[0] = cos(this.yaw) * cos(this.pitch);
+        this.dir[1] = sin(this.pitch);
+        this.dir[2] = sin(this.yaw) * cos(this.pitch);
+        // this.dir = normalized(this.dir);
+        this.right = normalized(cross(this.worldUp, this.dir));
+        this.up = cross(this.dir, this.right);
+    }
+
+    moveForward(dt) {
+        this.x += this.dir[0] * dt * this.speed;
+        this.z += this.dir[2] * dt * this.speed;
+    }
+
+    moveRight(dt) {
+        this.x += this.right[0] * dt * this.speed;
+        this.z += this.right[2] * dt * this.speed;
+    }
 }
 
 var keymap = {
@@ -68,9 +100,11 @@ var keymap = {
     d: false,
     shift: false,
     ' ': false,
+    lastMouseX: 0,
+    lastMouseY: 0
 }
 
-var cam = new Camera(-2, 1.3, -4, 0, 0, 0.005);
+var cam = new Camera(-2, 1.3, -4, 0, PI / 2, 0.005);
 
 document.onkeydown = e => {
     keymap[e.key.toLowerCase()] = true;
@@ -79,6 +113,27 @@ document.onkeydown = e => {
 
 document.onkeyup = e => {
     keymap[e.key.toLowerCase()] = false;
+}
+
+
+document.addEventListener("onmousemove", e => {
+    cam.yaw += e.movementX * 0.05;
+    keymap.lastMouseX = e.x;
+}, false);
+
+document.addEventListener('pointerlockchange', lockChange, false);
+
+function lockChange() {
+    if (document.pointerLockElement === canvas) {
+        document.addEventListener("mousemove", (e) => {
+            cam.yaw -= e.movementX * 0.002;
+            cam.pitch -= e.movementY * 0.002;
+        }, false);
+    }
+}
+
+canvas.onclick = e => {
+    canvas.requestPointerLock();
 }
 
 function beginRender() {
@@ -96,39 +151,44 @@ function beginRender() {
         c.clearRect(0, 0, width, height)
         let s = new Cube(the);
         // apply world transformations: translate, rotate about camera
-        let screenPts = s.points.map(e => [(e[0] - cam.x) / (e[2] - cam.z), (e[1] - cam.y) / (e[2] - cam.z)])
-
+        let view = cam.getViewMat();
+        let screenPts = s.points.map(e => view.vmul([e[0], e[1], e[2], 1]));
+        screenPts = screenPts.map(e => [(e[0]) / (e[2]), (e[1]) / (e[2])]);
+        // all the height/2 and width/2 additions is just the viewport transform
         for (var i = 0; i < 36; i += 3) {
             let ox = (screenPts[i][0] * width)
             let oy = -(screenPts[i][1]) * height
             c.beginPath()
-            c.moveTo(ox, oy)
+            c.moveTo(ox + width / 2, oy + height / 2)
             for (var j = 1; j < 3; j++) {
                 scrx = (screenPts[i + j][0] * width)
                 scry = -(screenPts[i + j][1]) * height
-                c.lineTo(scrx, scry)
-                c.fillRect(scrx, scry, 4, 4)
+                c.lineTo(scrx + width / 2, scry + height / 2)
+                c.fillRect(scrx + width / 2, scry + height / 2, 4, 4)
             }
-            c.lineTo(ox, oy)
+            c.lineTo(ox + width / 2, oy + height / 2)
             c.stroke()
         }
 
-        the += 0.04
+        // the += 0.04
         if (keymap.w) {
-            cam.z += cam.speed * dt;
+            cam.moveForward(dt);
         }
         if (keymap.s) {
-            cam.z -= cam.speed * dt;
+            cam.moveForward(-dt);
         }
         if (keymap.a) {
-            cam.x -= cam.speed * dt;
+            cam.moveRight(-dt);
         }
         if (keymap.d) {
-            cam.x += cam.speed * dt;
+            cam.moveRight(dt);
         }
         if (keymap.shift) cam.y -= cam.speed * dt * 0.8;
         if (keymap[' ']) cam.y += cam.speed * dt * 0.8;
-        setTimeout(() => requestAnimationFrame(render), 50);
+        c.fillRect(width / 2, height / 2, 3, 3);
+        // cam.yaw += 0.05;
+        cam.updateVecs();
+        setTimeout(() => requestAnimationFrame(render), 10);
     }
     requestAnimationFrame(render)
 }
@@ -193,29 +253,37 @@ class Cube {
     ]
     constructor(the) {
         for (var i = 0; i < 36; i++) {
-            this.points[i][0] -= 0.5
-            this.points[i][1] -= 0.5
-            this.points[i][2] -= 0.5
+            // this.points[i][0] -= 0.5
+            // this.points[i][1] -= 0.5
+            // this.points[i][2] -= 0.5
             this.points[i] = [this.points[i][0] * cos(the) + this.points[i][2] * sin(the), this.points[i][1], -this.points[i][0] * sin(the) + cos(the) * this.points[i][2]]
         }
     }
 }
 
 class Mat4 {
-    mat;
+    m;
     constructor() {
-        this.mat = Array(4).fill(Array(4).fill(0))
+        this.m = [...Array(4)].map(_ => Array(4).fill(0))
     }
 
     // since we're really just taking in another vector to perform this transformation, no need for a full mat4
-    mul(v) {
+    vmul(v) {
         let res = [0, 0, 0, 0]
-
-        return res.map((_, n) => v.map((e1, j) => e1 * this.mat[n][j]).reduce((su, ne) => su + ne));
+        return res.map((_, n) => v.map((e1, j) => e1 * this.m[n][j]).reduce((su, ne) => su + ne));
+    }
+    mmul(mat) {
+        let res = new Mat4();
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                res.m[i][j] = this.m[i].map((e, idx) => e * mat.m[idx][j]).reduce((x, y) => x + y);
+            }
+        }
+        return res;
     }
     static identity() {
         let m = new Mat4()
-        for (let i = 0; i < 4; i++) m.mat[i][i] = 1;
+        for (let i = 0; i < 4; i++) m.m[i][i] = 1;
         return m;
     }
 }
